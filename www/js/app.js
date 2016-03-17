5d4e4a4d6cd4d418b8adfa2b86d6570bae299740
jpdanttas <pedrodantas41@gmail.com>
2016-03-17 13:36:15 -0300

    view login update

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'starter.services', 'starter.factories', 'ksSwiper'])
+angular.module('starter', ['ionic',
+  'starter.controllers', 'starter.directives', 'starter.services', 'starter.factories',
+  'ngMaterial', 'ngMessages', 'ksSwiper'])
 
 .run(function($ionicPlatform, ConnectionService) {
   $ionicPlatform.ready(function() {
--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,10 +1,10 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', [ 'ngMaterial'])
 
 .controller('ConsumerSignupCtrl', ['$scope', '$http', '$state','Consumer', function($scope, $http, $state, Consumer) {
   var self = this;
   this.consumer = {};
 
-  this.signup = function(){
+  this.newConsumer = function() {
     console.log(self.consumer);
     Consumer.new(self.consumer).then(function(data){
       console.log('consumer added');
@@ -14,4 +14,56 @@ angular.module('starter.controllers', [])
         console.log('Dados duplicados');
     })
   };
-}]);
+}])
+
+.controller('ProviderSignupCtrl', ['$scope', '$http', '$state','Provider', function($scope, $http, $state, Provider) {
+  var self = this;
+  this.consumer = {};
+
+  this.newProvider = function(){
+    console.log(self.provider);
+    Provider.new(self.provider).then(function(data){
+      console.log('provider added');
+      $state.go('tutorial');
+    },function(err){
+      if(err.statusCode == 500)
+        console.log('Dados duplicados');
+    })
+  };
+}])
+
+.controller('TermsOfConsumers', function($scope, $mdDialog, $mdMedia) {
+  $scope.status = '  ';
+  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+  $scope.showAlert = function(ev) {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupTerms')))
+        .clickOutsideToClose(true)
+        .title('Termos')
+        .textContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus.' +
+          ' Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.')
+        .ariaLabel('Alert Dialog Demo')
+        .ok('Aceito')
+        .targetEvent(ev)
+    );
+  };
+})
+
+.controller('TermsOfProviders', function($scope, $mdDialog, $mdMedia) {
+  $scope.status = '  ';
+  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+  $scope.showAlert = function(ev) {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupTerms')))
+        .clickOutsideToClose(true)
+        .title('Termos')
+        .textContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus.' +
+          ' Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.')
+        .ariaLabel('Alert Dialog Demo')
+        .ok('Aceito')
+        .targetEvent(ev)
+    );
+  };
+});
